6e87eb4f68077688d027218a7482fb2a95e59308
Garfun <garfun1@gmail.com>
2021-03-31 19:33:33 +0300

    add field category in index

--- foodshop/shop/views.py
+++ foodshop/shop/views.py
@@ -4,8 +4,14 @@ from .models import Product, Category
 
 
 class ProductListView(ListView):
+    context_object_name = 'product_list'
     queryset = Product.objects.available()
     template_name = 'shop/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class ProductDetailView(DetailView):
