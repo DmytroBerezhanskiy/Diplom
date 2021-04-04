0d22211bfff9ffb20479ef6bb75b07c7fb7c695d
Garfun <garfun1@gmail.com>
2021-04-04 13:58:20 +0300

    class orderlist

--- foodshop/orderlist/orderlist.py
+++ foodshop/orderlist/orderlist.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from decimal import Decimal
+
 from foodshop.shop.models import Product
 
 
@@ -24,4 +25,30 @@ class OrderList(object):
 
     def save(self):
         self.session[settings.ORDERLIST_SESSION_ID] = self.orderlist
+        self.session.modified = True
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.orderlist:
+            del self.orderlist[product_id]
+            self.save()
+
+    def __iter__(self):
+        product_ids = self.orderlist.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.orderlist[str(product.id)]['product'] = product
+        for item in self.orderlist.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['count']
+            yield item
+
+    def __len__(self):
+        return sum(item['count'] for item in self.orderlist.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['count'] for item in self.orderlist.values())
+
+    def clear(self):
+        del self.session[settings.ORDERLIST_SESSION_ID]
         self.session.modified = True
