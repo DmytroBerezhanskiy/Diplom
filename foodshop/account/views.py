c8c55dd6a02cc723926dc3cf42fd023c0e6a5cf6
Garfun <garfun1@gmail.com>
2021-04-20 20:48:31 +0300

    add registartion

--- foodshop/account/views.py
+++ foodshop/account/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import RegistrationForm
 
+
+def register(request):
+    if request.method == 'POST':
+        register_form = RegistrationForm(request.POST)
+        if register_form.is_valid():
+            new_user = register_form.save(commit=False)
+            new_user.set_password(register_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        register_form = RegistrationForm()
+    return render(request, 'registration/register.html', {'register_form': register_form})
