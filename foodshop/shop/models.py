737aef788ad96539894eadb5b4dce5437f7b2c84
Garfun <garfun1@gmail.com>
2021-05-17 17:02:57 +0300

    pre save before creating answer

--- foodshop/shop/models.py
+++ foodshop/shop/models.py
@@ -39,6 +39,8 @@ class Category(models.Model):
     class Meta:
         ordering = ('name',)
         default_related_name = "category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
