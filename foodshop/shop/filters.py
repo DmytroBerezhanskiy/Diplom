66837721670adfa8bfb85bacd90ca241f0d5e7b4
Garfun <garfun1@gmail.com>
2021-06-16 20:16:13 +0300

    search icontains and price

--- foodshop/shop/filters.py
+++ foodshop/shop/filters.py
@@ -1,8 +1,15 @@
 import django_filters
+from django_filters import NumberFilter, RangeFilter, CharFilter
 from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
+    # price_start = NumberFilter(field_name='price', lookup_expr='gte')
+    # price_end = NumberFilter(field_name='price', lookup_expr='lte')
+    name = CharFilter(field_name='name', lookup_expr='icontains')
+    price = RangeFilter(field_name='price')
+
     class Meta:
         model = Product
         fields = ('category', 'name')
+        exclude = ['price']
